95e5a98b47b8d8812a2eb3217d3ca4970c5cd35c
Geovane_Andrade <geovanedeandradegrangeiro@gmail.com>
2023-11-09 16:12:14 -0300

    Feat: First commit

---
+++ src/main/java/com/maisprati/lojabicicleta/LojaBicicletaApplication.java
@@ -0,0 +1,13 @@
+package com.maisprati.lojabicicleta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaBicicletaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaBicicletaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maisprati/lojabicicleta/LojaBicicletaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maisprati.lojabicicleta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaBicicletaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
