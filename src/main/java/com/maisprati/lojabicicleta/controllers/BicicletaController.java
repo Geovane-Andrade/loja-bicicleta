8baaa2df4dfb1ea12b1862fe984b46df86f86457
Geovane_Andrade <geovanedeandradegrangeiro@gmail.com>
2023-11-22 19:10:10 -0300

    Feat: Criacao do controller

---
+++ src/main/java/com/maisprati/lojabicicleta/controllers/BicicletaController.java
@@ -0,0 +1,58 @@
+package com.maisprati.lojabicicleta.controllers;
+
+import com.maisprati.lojabicicleta.domain.models.Bicicleta;
+import com.maisprati.lojabicicleta.requests.BicicletaPostRequestBody;
+import com.maisprati.lojabicicleta.requests.BicicletaPutRequestBody;
+import com.maisprati.lojabicicleta.service.BicicletaService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("api/v1/bicicletas")
+public class BicicletaController {
+
+    private final BicicletaService bicicletaService;
+
+
+    public BicicletaController(BicicletaService bicicletaService) {
+        this.bicicletaService = bicicletaService;
+    }
+
+    @GetMapping
+    public List<Bicicleta> listarTodasBicicletas() {
+        return bicicletaService.listAll();
+    }
+
+    @GetMapping("/{id}")
+    public Bicicleta listarPorId(@PathVariable UUID id) {
+        return bicicletaService.findByIdOrThrowBadRequestException(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Bicicleta> criarBicicleta(@RequestBody BicicletaPostRequestBody bicicletaPostRequestBody) throws Exception {
+        return new ResponseEntity<>(bicicletaService.save(bicicletaPostRequestBody), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarBicicleta(@PathVariable UUID id) {
+        bicicletaService.delete(id);
+        return new ResponseEntity<>( HttpStatus.NO_CONTENT);
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> atualizarBicicleta(@RequestBody BicicletaPutRequestBody bicicletaPutRequestBody) throws Exception {
+        bicicletaService.replace(bicicletaPutRequestBody);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+//    @PatchMapping("/{id}")
+//    public ResponseEntity<Bicicleta> atualizarNomeBicicleta(@PathVariable UUID id,
+//                                                            @RequestBody Bicicleta bicicleta) throws Exception {
+//        bicicletaService.update(bicicleta);
+//        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+//    }
+
+}
